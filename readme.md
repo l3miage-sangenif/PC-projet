3a63377cf0204e7cde33cb52a3d0aac33ac9e901
floreal15 <floreal.sangenis@algoo.fr>
2023-11-19 21:17:35 +0100

    fix bug

--- readme.md
+++ readme.md
@@ -1,9 +1,1 @@
 # mvn -X clean verify sonar:sonar -Dsonar.login=f4c6a1b02cafefeec22653591458c2ee46daf33c 
-
-a faire : 
--mettre shape to move en list pour pouvoir faire les groupement
--faire les groupements
--corriger dette technique du aux setter 
--fix bug ctrl+z et ctrl+y avec move
-
--mettre les export dans des commande ( et faire les undo (surement en sauvgardant l'ancienne version))
--- src/main/java/edu/uga/miage/m1/polygons/gui/command/CommandMoveShape.java
+++ src/main/java/edu/uga/miage/m1/polygons/gui/command/CommandMoveShape.java
@@ -8,17 +8,23 @@ import edu.uga.miage.m1.polygons.gui.shapes.SimpleShape;
 
 public class CommandMoveShape implements Command {
 
-    private SimpleShape shape;
+    private ListShapes shapes;
     private int coordXMove;
     private int coordYMove;
     MouseEvent evt;
-    int coordXinit;
-    int coordYInit;
+    int[] coordXinit;
+    int[] coordYInit;
 
-    public CommandMoveShape(SimpleShape shape, int xInit, int yInit, MouseEvent evt) {
-        coordXinit=shape.getX();
-        coordYInit=shape.getY();
-        this.shape=shape;
+    public CommandMoveShape(ListShapes shape, int xInit, int yInit, MouseEvent evt) {
+        coordXinit= new int[shape.getList().size()];
+        coordYInit= new int[shape.getList().size()];
+        int i=0;
+        for (SimpleShape s : shape.getList()) {
+            coordXinit[i]=s.getX();
+            coordYInit[i]=s.getY();
+            i++;
+        }
+        this.shapes=shape;
         this.coordXMove=xInit;
         this.coordYMove=yInit;
         this.evt=evt;
@@ -26,13 +32,19 @@ public class CommandMoveShape implements Command {
 
     @Override
     public void execute(ListShapes list, Graphics2D g2) {
-        list.moveShape(shape, shape.getX()+evt.getX()-coordXMove, shape.getY()+evt.getY()-coordYMove);
+        for (SimpleShape shape : shapes.getList()) {
+            list.moveShape(shape, shape.getX()+evt.getX()-coordXMove, shape.getY()+evt.getY()-coordYMove);
+        }
         list.executeAll(g2);
     }
 
     @Override
     public void undo(ListShapes list, Graphics2D g2) {
-        list.moveShape(shape, coordXinit, coordYInit);
+        int i=0;
+        for (SimpleShape shape : shapes.getList()) {
+            list.moveShape(shape, coordXinit[i], coordYInit[i]);
+            i++;
+        }
         list.executeAll(g2);
     }
 
